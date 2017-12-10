018f8cf1cfead002eab76c8889fd745cc37bbaa1
yijunjun <junjunyi@gmail.com>
2017-12-10 22:08:53 +0800

    修改reset函数及读配置文件

--- main.go
+++ main.go
@@ -5,16 +5,38 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"net"
 	"time"
 
-	"github.com/astaxie/beego/config"
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/layers"
 	"github.com/google/gopacket/pcap"
 )
+
+type config struct {
+	Domains []string
+	Ips     []string
+}
+
+func loadConfig(filePath string) (*config, error) {
+	bs, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		return nil, err
+	}
+
+	c := &config{}
+
+	err = json.Unmarshal(bs, c)
+	if err != nil {
+		return nil, err
+	}
+
+	return c, nil
+}
 
 var gHandle *pcap.Handle
 
@@ -22,7 +44,7 @@ func main() {
 	confFilePath := flag.String("config", "conf.json", "config file json format")
 	flag.Parse()
 
-	jsonConf, err := config.NewConfig("json", *confFilePath)
+	jsonConf, err := loadConfig(*confFilePath)
 	if err != nil {
 		fmt.Println(err.Error())
 		return
@@ -30,19 +52,19 @@ func main() {
 
 	blackIPMap := make(map[string]struct{})
 
-	for _, ds := range jsonConf.Strings("domains") {
+	for _, ds := range jsonConf.Domains {
 		ips, err := net.LookupIP(ds)
 		if err != nil {
 			fmt.Println(ds, err.Error())
 			continue
 		}
+		fmt.Println(ds, ips)
 		for _, ip := range ips {
 			blackIPMap[ip.String()] = struct{}{}
 		}
-
 	}
 
-	for _, ipStr := range jsonConf.Strings("ips") {
+	for _, ipStr := range jsonConf.Ips {
 		ip := net.ParseIP(ipStr)
 		if ip != nil {
 			blackIPMap[ip.String()] = struct{}{}
@@ -73,7 +95,7 @@ func main() {
 		return
 	}
 
-	gHandle, err = pcap.OpenLive(devName, 1024, false, 10*time.Second)
+	gHandle, err = pcap.OpenLive(devName, 64, true, pcap.BlockForever)
 	if err != nil {
 		fmt.Println(err.Error())
 		return
@@ -82,7 +104,7 @@ func main() {
 
 	fmt.Println("listen device", devName)
 
-	err = gHandle.SetBPFFilter("tcp and port 80")
+	err = gHandle.SetBPFFilter("tcp and (dst port 80 or dst port 443)")
 	if err != nil {
 		fmt.Println(err.Error())
 		return
@@ -96,7 +118,6 @@ func main() {
 			if _, ok := blackIPMap[ip.DstIP.String()]; ok {
 				reset(p, ip)
 			}
-
 		}
 	}
 
@@ -116,27 +137,50 @@ func reset(pack gopacket.Packet, ip *layers.IPv4) {
 		return
 	}
 	tcp, _ := tcpLayer.(*layers.TCP)
+	if !tcp.SYN {
+		return
+	}
+
+	tcpDataLen := uint32(ip.Length - uint16(ip.IHL)*4)
+	tcpDataLen -= uint32((tcp.DataOffset & 0x0F) * 4)
 
 	buf := gopacket.NewSerializeBuffer()
-	opts := gopacket.SerializeOptions{}
+	opts := gopacket.SerializeOptions{
+		FixLengths:       true,
+		ComputeChecksums: true,
+	}
 	gopacket.SerializeLayers(buf, opts,
 		&layers.Ethernet{
-			SrcMAC: eth.DstMAC,
-			DstMAC: eth.SrcMAC,
+			SrcMAC:       eth.DstMAC,
+			DstMAC:       eth.SrcMAC,
+			EthernetType: eth.EthernetType,
 		},
 		&layers.IPv4{
-			SrcIP: ip.DstIP,
-			DstIP: ip.SrcIP,
+			Version:  4,
+			TTL:      255,
+			TOS:      ip.TOS,
+			SrcIP:    ip.DstIP,
+			DstIP:    ip.SrcIP,
+			Protocol: ip.Protocol,
 		},
 		&layers.TCP{
 			SrcPort: tcp.DstPort,
 			DstPort: tcp.SrcPort,
-			Ack:     tcp.Seq - 1,
+			Ack:     tcp.Seq + tcpDataLen + 1,
+			Seq:     tcp.Ack,
 			RST:     true,
+			ACK:     true,
+			PSH:     true,
+			URG:     true,
 		},
 	)
-	err := gHandle.WritePacketData(buf.Bytes())
+
+	bbs := buf.Bytes()
+	fmt.Println(len(bbs), bbs)
+	err := gHandle.WritePacketData(bbs) //buf.Bytes())
 	if err != nil {
-		fmt.Println(ip.DstIP.String(), err.Error())
+		fmt.Println("error:", ip.DstIP.String(), err.Error())
 	}
+
+	fmt.Println(time.Now().Unix(), ip.DstIP.String(), ip.Length, ip.IHL*4, tcp.DataOffset&0x0F, tcpDataLen)
 }
